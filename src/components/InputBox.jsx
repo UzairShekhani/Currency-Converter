d5bae1ba28332ccefa50fe05753253a877473ab9
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-03-29 23:24:07 +0500

    ui updated

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function InputBox( {
-   
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption = [],
+    selectCurrency = "",
+    amountDisble = false,
+    CurruncyDisble = false,
+    className = "",
     
 }){
 
@@ -18,7 +26,7 @@ function InputBox( {
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className='text-black/40 mb-2 w-full'>Currency Type</p>
-                <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none' value={selectCurrency} onChange={(e.)}>
+                <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none' value={selectCurrency} onChange={(e)}>
                     <option value="usd">
                         usd
                     </option>

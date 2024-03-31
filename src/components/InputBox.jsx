20a9b35dd2dc2479b56761f854e4e70517b1d9f6
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-03-31 16:32:35 +0500

    ui update

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -24,14 +24,7 @@ function InputBox( {
                  disabled={amountDisble}
                  value={amount} onChange={(e) => onAmountChange && onAmountChange( Number(e.target.value))} />
             </div>
-            <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className='text-black/40 mb-2 w-full'>Currency Type</p> 
-                <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none' value={selectCurrency} onChange={(e)}>
-                    <option value="usd">
-                        usd
-                    </option>
-                </select>
-            </div>
+            
         </div>
     )
 }
